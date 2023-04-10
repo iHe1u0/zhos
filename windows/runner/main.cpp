004abd170f710b0db5ffb1a15fd361012bc2fe8d
iMorning <morningos@foxmail.com>
2023-04-10 18:37:10 +0800

    change windows title

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -1,15 +1,18 @@
 #include <flutter/dart_project.h>
 #include <flutter/flutter_view_controller.h>
 #include <windows.h>
-
+#include <locale>
 #include "flutter_window.h"
 #include "utils.h"
 
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
-                      _In_ wchar_t *command_line, _In_ int show_command) {
+                      _In_ wchar_t *command_line, _In_ int show_command)
+{
+  setlocale(LC_CTYPE, "chs");
   // Attach to console when present (e.g., 'flutter run') or create a
   // new console when running with a debugger.
-  if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
+  if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent())
+  {
     CreateAndAttachConsole();
   }
 
@@ -24,16 +27,22 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
+  // Default window configuration
+  auto windowTitle = TEXT("zhos.net");
+  UINT windowHeight = GetSystemMetrics(SM_CYSCREEN);
+  UINT windowWidth = GetSystemMetrics(SM_CXSCREEN);
   FlutterWindow window(project);
-  Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
-  if (!window.Create(L"os", origin, size)) {
+  Win32Window::Point origin(0, 0);
+  Win32Window::Size size(windowWidth, windowHeight);
+  if (!window.Create(windowTitle, origin, size))
+  {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
 
   ::MSG msg;
-  while (::GetMessage(&msg, nullptr, 0, 0)) {
+  while (::GetMessage(&msg, nullptr, 0, 0))
+  {
     ::TranslateMessage(&msg);
     ::DispatchMessage(&msg);
   }
--- windows/runner/utils.cpp
+++ windows/runner/utils.cpp
@@ -1,5 +1,6 @@
-#include "utils.h"
+#pragma warning(disable : 4267)
 
+#include "utils.h"
 #include <flutter_windows.h>
 #include <io.h>
 #include <stdio.h>
@@ -7,13 +8,17 @@
 
 #include <iostream>
 
-void CreateAndAttachConsole() {
-  if (::AllocConsole()) {
+void CreateAndAttachConsole()
+{
+  if (::AllocConsole())
+  {
     FILE *unused;
-    if (freopen_s(&unused, "CONOUT$", "w", stdout)) {
+    if (freopen_s(&unused, "CONOUT$", "w", stdout))
+    {
       _dup2(_fileno(stdout), 1);
     }
-    if (freopen_s(&unused, "CONOUT$", "w", stderr)) {
+    if (freopen_s(&unused, "CONOUT$", "w", stderr))
+    {
       _dup2(_fileno(stdout), 2);
     }
     std::ios::sync_with_stdio();
@@ -21,18 +26,21 @@ void CreateAndAttachConsole() {
   }
 }
 
-std::vector<std::string> GetCommandLineArguments() {
+std::vector<std::string> GetCommandLineArguments()
+{
   // Convert the UTF-16 command line arguments to UTF-8 for the Engine to use.
   int argc;
-  wchar_t** argv = ::CommandLineToArgvW(::GetCommandLineW(), &argc);
-  if (argv == nullptr) {
+  wchar_t **argv = ::CommandLineToArgvW(::GetCommandLineW(), &argc);
+  if (argv == nullptr)
+  {
     return std::vector<std::string>();
   }
 
   std::vector<std::string> command_line_arguments;
 
   // Skip the first argument as it's the binary name.
-  for (int i = 1; i < argc; i++) {
+  for (int i = 1; i < argc; i++)
+  {
     command_line_arguments.push_back(Utf8FromUtf16(argv[i]));
   }
 
@@ -41,15 +49,18 @@ std::vector<std::string> GetCommandLineArguments() {
   return command_line_arguments;
 }
 
-std::string Utf8FromUtf16(const wchar_t* utf16_string) {
-  if (utf16_string == nullptr) {
+std::string Utf8FromUtf16(const wchar_t *utf16_string)
+{
+  if (utf16_string == nullptr)
+  {
     return std::string();
   }
   int target_length = ::WideCharToMultiByte(
       CP_UTF8, WC_ERR_INVALID_CHARS, utf16_string,
       -1, nullptr, 0, nullptr, nullptr);
   std::string utf8_string;
-  if (target_length == 0 || target_length > utf8_string.max_size()) {
+  if (target_length == 0 || target_length > utf8_string.max_size())
+  {
     return utf8_string;
   }
   utf8_string.resize(target_length);
@@ -57,8 +68,23 @@ std::string Utf8FromUtf16(const wchar_t* utf16_string) {
       CP_UTF8, WC_ERR_INVALID_CHARS, utf16_string,
       -1, utf8_string.data(),
       target_length, nullptr, nullptr);
-  if (converted_length == 0) {
+  if (converted_length == 0)
+  {
     return std::string();
   }
   return utf8_string;
+}
+
+std::wstring stringToWstring(const std::string &str)
+{
+  std::wstring result;
+  size_t charWidth = 0;
+  int len = MultiByteToWideChar(CP_ACP, 0, str.c_str(), str.size(), NULL, charWidth);
+  TCHAR *buffer = new TCHAR[len + 1];
+  MultiByteToWideChar(CP_ACP, charWidth, str.c_str(), str.size(), buffer, len);
+  buffer[len] = '\0';
+  result.append(buffer);
+  delete[] buffer;
+  buffer = NULL;
+  return result;
 }
